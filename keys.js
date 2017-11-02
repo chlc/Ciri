e926b342f1e14203b8335901cc0a4d1d428b644f
chlc <7246337+chlc@users.noreply.github.com>
2017-11-02 18:03:52 -0500

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log("this is loaded");
+
+exports.twitterKeys = {
+  consumer_key: 't96fLFzuDOFHoGErlrCeldEK8',
+  consumer_secret: 'iCnSPRudHeQt14YOW2eYLGp6vbYFC2oBnhnIWvYcImOgFTXrK4',
+  access_token_key: '139284033-AMhFFREpGlHcFY8t63P2FgMInETu4qlDn7HyR8jg',
+  access_token_secret: 'Qp3vy6Yi6WdLnc9bdrLf8iiujgyXoSdUVs29C0LHDsIDm',
+}
+
